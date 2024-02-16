ebbbdb4be09ad82085aaf14f2d6a45137c1d3106
nomi-kappa <koutsoubari_nomiki@yahoo.com>
2024-02-16 15:16:47 +0000

    polynomial and knots aroun 20

--- token.R
+++ token.R
@@ -12,3 +12,13 @@ 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 (classic for R_TUS) 28/9
 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 - R_TUS classic 28/10
 
 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 - R classic 28/10
+
+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 9/11/2023
+
+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 9/11/23 
+
+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 9/12/23
+
+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 15/01/2024
+
+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 17/03/24
